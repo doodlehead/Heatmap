bc1b4922154a3711253a5018147b8167c61f346f
Frank Yu <frankyu1999@gmail.com>
2020-02-09 09:50:10 -0500

    save

--- src/components/Heatmap.js
+++ src/components/Heatmap.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import { Get } from 'react-axios'
 
 class Heatmap extends Component {
 
@@ -9,24 +10,14 @@ class Heatmap extends Component {
       lat: 45.4215,
       lng: -75.6972
     },
-    zoom: 5
+    zoom: 3,
+    heatmapOptions: {
+      radius: 20,
+      opacity: 0.6
+    }
   };
 
-  getHeatmapData = () => {
-    const testData = [
-      { lat: 55.5, lng: 34.56 },
-      { lat: 34.7, lng: 28.4 }
-    ]
-
-    return {
-      positions: this.generatePositions(10000),
-      options: {
-        radius: 20,
-        opacity: 0.6
-      }
-    }
-  }
-
+  //Generate test data
   generatePositions = (length) => {
     const res = [];
     let lat = 0;
@@ -40,33 +31,76 @@ class Heatmap extends Component {
     return res;
   }
 
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      heatMapData: this.getHeatmapData()
-    }
-    //Fetch the heatmap data?
-  }
-
   handleApiLoaded = (map, maps) => {
     console.log(map);
     console.log(maps);
   }
 
+  //Format useful data
+  formatData = (response) => {
+    const res = []
+    response.data.forEach(elem => {
+      const position = {}
+      if (elem.status_place) {
+        //Not worth it yet
+        return
+      } else if (elem.user_country) {
+        //[longitude, latitude]
+        position.lng = elem.user_country[0]
+        position.lat = elem.user_country[1]
+      }
+
+      //Weight of the data-point on the map depends on the sentiment score
+      if (elem.sentiment_score < 0) {
+        position.weight = Math.abs(elem.sentiment_score)
+      } else {
+        //Don't show positive
+        return
+      }
+
+      //Add the data point
+      res.push(position)
+    });
+    console.log(res)
+
+    //Show either positive or negative?
+    return res;
+  }
+  /*
+  Heatmap position data should be in this format:
+  {
+    lat: Number,
+    lng: Number,
+    weight: Number,
+  }
+  */
   render() {
     return (
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyANE_oRMmlJn8potIPdqYUCf3-4KUQZ490' }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-          yesIWantToUseGoogleMapApiInternals
-          onGoogleApiLoaded={({ map, maps }) => this.handleApiLoaded(map, maps)}
-          heatmapLibrary={true}
-          heatmap={this.state.heatMapData ? this.state.heatMapData : {}}
-        ></GoogleMapReact>
-      </div>
+      <Get url="http://localhost:5000/posts-useful">
+        {(error, response, isLoading, makeRequest, axios) => {
+          if (!isLoading && response) {
+            console.log(response)
+            return (
+              <div style={{ height: '100vh', width: '100%' }}>
+                <GoogleMapReact
+                  bootstrapURLKeys={{ key: process.env.GOOGLE_MAPS_API_KEY }}
+                  defaultCenter={this.props.center}
+                  defaultZoom={this.props.zoom}
+                  yesIWantToUseGoogleMapApiInternals
+                  onGoogleApiLoaded={({ map, maps }) => this.handleApiLoaded(map, maps)}
+                  heatmapLibrary={true}
+                  heatmap={{
+                    positions:this.formatData(response),
+                    options: this.props.heatmapOptions
+                  }}
+                ></GoogleMapReact>
+              </div>
+            )
+          } else {
+            return <div>Loading...</div>
+          }
+        }}
+      </Get>
     )
   }
 }
